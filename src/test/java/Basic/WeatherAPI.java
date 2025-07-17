5993d069d8e619aaaa00e56925a73e60c89efa4a
Administrator <sibusiso.nkwanyana@simplify.africa>
2025-07-17 20:14:54 +0200

    Added updateWeatherStation method

--- src/test/java/Basic/WeatherAPI.java
+++ src/test/java/Basic/WeatherAPI.java
@@ -3,11 +3,21 @@ package Basic;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+import java.util.PriorityQueue;
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) // orders tests alphabetically
 public class WeatherAPI {
+
+    static String weatherStationId;
+
     @Test
-    public void createWeatherStation() {
+    public void a_createWeatherStation() {
         String baseUrl = "http://api.openweathermap.org";
         String path = "/data/3.0/stations";
         String apiKey = "b1589ee5727295072e2272d60dfc6904";
@@ -24,26 +34,58 @@ public class WeatherAPI {
                 .basePath(path)
                 .contentType(ContentType.JSON)
                 .queryParam("appid", apiKey)
+                .log().all()
                 .body(payload)
                 .post();
 
-        System.out.println("Status Code: " + response.getStatusCode());
-        System.out.println("Response Body: " + response.getBody().asString());
+        int actualStatusCode =response.getStatusCode();
+        Assert.assertEquals(actualStatusCode,201);
+
+        weatherStationId = response.jsonPath().getString("ID");
     }
 
     @Test
-    public void getWeatherStation() {
+    public void b_getWeatherStation() {
         String baseUrl = "http://api.openweathermap.org";
-        String path = "/data/3.0/stations/6879279bcbd4230001cc0564";
+        String path = "/data/3.0/stations/" + weatherStationId;
         String apiKey = "b1589ee5727295072e2272d60dfc6904";
+
+         RestAssured.given()
+                .baseUri(baseUrl)
+                .basePath(path)
+                .queryParam("appid", apiKey)
+                .log().all()
+                .get()
+                .then()
+                .statusCode(200);
+
+
+    }
+
+    @Test
+    public void c_updateWeatherStation(){
+        String baseUrl = "http://api.openweathermap.org";
+        String path = "/data/3.0/stations/" + weatherStationId;
+        String apiKey = "b1589ee5727295072e2272d60dfc6904";
+
+        String payload = "{\n" +
+                "  \"external_id\": \"SF_TEST001\",\n" +
+                "  \"name\": \"Updated weather station\",\n" +
+                "  \"latitude\": 37.76,\n" +
+                "  \"longitude\": -122.43,\n" +
+                "  \"altitude\": 150\n" +
+                "}";
 
         Response response = RestAssured.given()
                 .baseUri(baseUrl)
                 .basePath(path)
+                .contentType(ContentType.JSON)
                 .queryParam("appid", apiKey)
-                .get();
+                .log().all()
+                .body(payload)
+                .put();
 
-        System.out.println("Status Code: " + response.getStatusCode());
-        System.out.println("Response Body: " + response.getBody().asString());
+        int actualStatusCode =response.getStatusCode();
+        Assert.assertEquals(actualStatusCode,200);
     }
 }
