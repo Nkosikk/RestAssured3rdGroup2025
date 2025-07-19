c2573ab00f20ed90e5ad9c353738e084413ac180
lence <lmlence@gmail.com>
2025-07-19 18:39:28 +0200

    Added a getUser method

--- src/test/java/Basic/ReqresAPI.java
+++ src/test/java/Basic/ReqresAPI.java
@@ -7,8 +7,10 @@ import org.junit.Test;
 
 public class ReqresAPI {
 
+
+
     @Test
-    public void createUser() {
+    public void a_createUser() {
         String baseUrl = "https://reqres.in";
         String path = "/api/users/";
         String apiKey = "reqres-free-v1";
@@ -26,5 +28,31 @@ public class ReqresAPI {
 
         response.then().log().all();
         Assert.assertEquals(201, response.getStatusCode());
+
     }
+
+    @Test
+    public void b_getUser() {
+
+        String baseUrl = "https://reqres.in";
+        String path = "/api/users/2";
+        String apiKey = "reqres-free-v1";
+
+        Response response = RestAssured.given()
+                .baseUri(baseUrl)
+                .basePath(path)
+                .header("x-api-key", apiKey)
+                .log().all()
+                .get();
+
+        response.then().log().all();
+        Assert.assertEquals(200, response.getStatusCode());
+    }
+
+
+
+
+
+
+
 }
