dada5fac800b38f4b1e9083f6e9672aa2ebf281f
Administrator <sibusiso.nkwanyana@simplify.africa>
2025-07-24 19:52:03 +0200

    Added Authorisations BasePaths OpenWeatherPayloadBuilder OpenWeatherRequestBuilder OpenWeatherTest

---
+++ src/test/java/Tests/OpenWeatherTest.java
@@ -0,0 +1,19 @@
+package Tests;
+
+import RequestBuilder.OpenWeatherRequestBuilder;
+import org.testng.annotations.Test;
+
+public class OpenWeatherTest {
+
+    @Test
+    public void createWeatherStationTest(){
+        /**This test is creating a new weather station */
+        OpenWeatherRequestBuilder.createOpenWeatherResponse()
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(201)
+                .contentType("application/json; charset=utf-8");
+    }
+}
