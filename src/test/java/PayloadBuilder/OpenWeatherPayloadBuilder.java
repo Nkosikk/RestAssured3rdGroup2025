aee57fd7c525b6afd149978607afb6e802076a5f
lence <lmlence@gmail.com>
2025-07-26 20:24:15 +0200

    added createOpenWeatherResponse createWeatherStationBody

--- src/test/java/PayloadBuilder/OpenWeatherPayloadBuilder.java
+++ src/test/java/PayloadBuilder/OpenWeatherPayloadBuilder.java
@@ -1,7 +1,25 @@
 package PayloadBuilder;
 
+import org.json.simple.JSONObject;
+import org.junit.Test;
+
+
 public class OpenWeatherPayloadBuilder {
 
+    /**
+     * This method creates a JSON payload for creating a weather station.
+     * @return JSONObject representing the weather station details.
+     */
+    public static JSONObject  createWeatherStationBody(){
+        JSONObject station = new JSONObject();
+        station.put("external_id", "SF_TEST001124");
+        station.put("name", "San Francisco Test Station");
+        station.put("latitude", 37.76);
+        station.put("longitude", -122.43);
+        station.put("altitude", 150);
+
+        return station;
+    }
 
 
 }
--- src/test/java/RequestBuilder/OpenWeatherRequestBuilder.java
+++ src/test/java/RequestBuilder/OpenWeatherRequestBuilder.java
@@ -1,4 +1,34 @@
 package RequestBuilder;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
+
+import static Common.Authorisation.openWeatherApiKey;
+import static Common.BasePaths.openWeatherBaseUrl;
+import static Common.BasePaths.openWeatherPath;
+
 public class OpenWeatherRequestBuilder {
+
+    static String weatherStationId;
+    public static Response createOpenWeatherResponse(){
+
+        Response response = RestAssured.given()
+                .baseUri(openWeatherBaseUrl)
+                .basePath(openWeatherPath)
+                .contentType(ContentType.JSON)
+                .queryParam("appid", openWeatherApiKey)
+                .log().all()
+//              .body(PayloadBuilder.OpenWeatherPayloadBuilder.createWeatherStationBody())
+                .body(createOpenWeatherResponse())
+                .post()
+                .then()
+                .extract().response();
+
+        weatherStationId = response.jsonPath().getString("ID");
+        return response;
+
+
+    }
 }
