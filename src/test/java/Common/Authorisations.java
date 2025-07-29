7a6d6aa7b137db734ca909d8ce025d2944d693d5
cmokale <cideebay@gmail.com>
2025-07-29 15:50:33 +0200

    Added Pakages and classes for WeatherAPI

---
+++ src/test/java/Common/Authorisations.java
@@ -0,0 +1,6 @@
+package Common;
+
+public class Authorisations {
+    public static String openWeatherApiKey = "b1589ee5727295072e2272d60dfc6904";
+}
+
---
+++ src/test/java/PayloadBuilder/OpenWeatherPayloadBuilder.java
@@ -0,0 +1,19 @@
+package PayloadBuilder;
+
+import org.json.simple.JSONObject;
+
+public class OpenWeatherPayloadBuilder {
+
+    public static JSONObject createWeatherStationBody(){
+
+        JSONObject station = new JSONObject();
+        station.put("external_id","ext station id");
+        station.put("name","First station");
+        station.put("latitude",37.76);
+        station.put("longitude",-122.43);
+        station.put("altitude",230);
+
+        return station;
+    }
+}
+
