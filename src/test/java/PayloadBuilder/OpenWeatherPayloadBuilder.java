732f2b98069bd2f099b70638984eef5093f7cd20
Administrator <sibusiso.nkwanyana@simplify.africa>
2025-07-29 20:04:57 +0200

    Added TestDataGenerator class with its fields

--- src/test/java/PayloadBuilder/OpenWeatherPayloadBuilder.java
+++ src/test/java/PayloadBuilder/OpenWeatherPayloadBuilder.java
@@ -15,4 +15,28 @@ public class OpenWeatherPayloadBuilder {
 
         return station;
     }
+
+    public static JSONObject updateWeatherStationBody(){
+
+        JSONObject station = new JSONObject();
+        station.put("external_id","ext station id");
+        station.put("name","Update first station");
+        station.put("latitude",37.76);
+        station.put("longitude",-122.43);
+        station.put("altitude",230);
+
+        return station;
+    }
+
+    public static JSONObject createWeatherStationWithoutNameBody(){
+
+        JSONObject station = new JSONObject();
+        station.put("external_id","ext station id");
+        station.put("name","");
+        station.put("latitude",37.76);
+        station.put("longitude",-122.43);
+        station.put("altitude",230);
+
+        return station;
+    }
 }
