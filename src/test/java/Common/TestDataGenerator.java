d7a89b73991b1bd3523bcec4c1458cc62501fe3b
bmahinga <shiluvam@gmail.com>
2025-07-31 16:18:45 +0200

    creted New Package named stations and class for test data generator

--- src/test/java/Common/TestDataGenerator.java
+++ src/test/java/Common/TestDataGenerator.java
@@ -4,11 +4,16 @@ import com.github.javafaker.Faker;
 import org.json.simple.JSONObject;
 
 public class TestDataGenerator {
-
-
-
-
-
+    public  static String externalId = Faker.instance().idNumber().valid();
+    public static String stationName = Faker.instance().name().firstName() + " Weather Station";
+    public static String longitude = Faker.instance().address().longitude();
+    public static String latitude = Faker.instance().address().latitude();
+    public static int altitude = Faker.instance().number().numberBetween(10,20);
 
 }
+
+
+
+
+
 
--- src/test/java/PayloadBuilder/OpenWeatherPayloadBuilder.java
+++ src/test/java/PayloadBuilder/OpenWeatherPayloadBuilder.java
@@ -1,18 +1,24 @@
 package PayloadBuilder;
 
+import com.github.javafaker.Faker;
 import org.json.simple.JSONObject;
 
-
+import static Common.TestDataGenerator.*;
 public class OpenWeatherPayloadBuilder {
 
     public static JSONObject createWeatherStationBody(){
 
         JSONObject station = new JSONObject();
-        station.put("external_id","ext station id");
-        station.put("name","First station");
-        station.put("latitude",37.76);
-        station.put("longitude",-122.43);
-        station.put("altitude",230);
+
+        station.put("external_id",externalId);
+        station.put("name",stationName);
+        //station.put("latitude",Double.parseDouble(Faker));
+        String latitudeStr = Faker.instance().address().latitude().replace(",", ".");
+        double latitude = Double.parseDouble(latitudeStr);
+//        station.put("latitude", Double.parseDouble(Faker.instance().address().latitude()));
+//        station.put("longitude", Double.parseDouble(Faker.instance().address().longitude()));
+        //station.put("longitude",longitude);
+        station.put("altitude",altitude);
 
         return station;
     }
