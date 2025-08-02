88cea0e60d2ea3903a367db978702e902a7e8525
Amanda <simangeleramavhale@gmail.com>
2025-08-02 22:42:12 +0200

    aaded  methods on class

--- src/test/java/Stations/WeatherAPI.java
+++ src/test/java/Stations/WeatherAPI.java
@@ -1,7 +1,11 @@
 package Stations;
 
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.http.ContentType;
+
+import static Common.Authorisations.openWeatherApiKey;
+import static Common.BasePaths.openWeatherBaseUrl;
 import static io.restassured.RestAssured.given;
 import org.junit.Test;
 
@@ -20,6 +24,58 @@ public class WeatherAPI {
                 .body("{\"external_id\": \"Gauteng\", \"name\": \"POP trust\", \"latitude\": 37.76, \"longitude\": -122.43, \"altitude\": 123}")
                 .post();
         response.then().statusCode(201);
+        System.out.println(response.asString());
+    }
+    @Test
+    public void getStationTest() {
+        RestAssured.given()
+                .baseUri(openWeatherBaseUrl)
+                .basePath(PATH_ID)
+                .queryParam("appid", openWeatherApiKey)
+                .log().all()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+    }
+    @Test
+
+    public void updateStationTest() {
+        Response response = given()
+                .baseUri(BASE_URL)
+                .basePath(PATH_ID)
+                .queryParam("appid", APPID)
+                .contentType(ContentType.JSON)
+                .body("{\"external_id\": \"Gauteng\", \"name\": \"POP trust\", \"latitude\": 37.76, \"longitude\": -122.43, \"altitude\": 123}")
+                .put();
+        response.then().statusCode(201);
+        System.out.println(response.asString());
+
+    }
+    @Test
+    public void deleteStationTest() {
+        RestAssured.given()
+                .baseUri(openWeatherBaseUrl)
+                .basePath(PATH_ID)
+                .queryParam("appid", openWeatherApiKey)
+                .log().all()
+                .delete()
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+    }
+    @Test
+    public void negativeStationTest() {
+        Response response = given()
+                .baseUri(BASE_URL)
+                .basePath(PATH)
+                .queryParam("appid", APPID)
+                .contentType(ContentType.JSON)
+                .body("{\"external_id\": \"Gauteng\", \"name\": \"POP trust\", \"latitude\": 37.76, \"longitude\": -122.43, \"altitude\": 123}")
+                .post();
+        response.then().statusCode(400);
         System.out.println(response.asString());
     }
 }
