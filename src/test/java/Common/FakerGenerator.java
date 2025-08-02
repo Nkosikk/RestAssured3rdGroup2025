68ba6831f893fe5aabadf732bc0fb781aca63fc5
Amanda <simangeleramavhale@gmail.com>
2025-08-02 22:39:17 +0200

    aaded  methods on Test class

---
+++ src/test/java/Common/FakerGenerator.java
@@ -0,0 +1,13 @@
+package Common;
+
+import com.github.javafaker.Faker;
+
+public class FakerGenerator {
+
+    public static double longitude = Double.parseDouble(Faker.instance().address().longitude());
+    public static double latidude = Double.parseDouble(Faker.instance().address().latitude());
+    public static int altidude = Faker.instance().number().numberBetween(10,20);
+
+}
+
+
--- src/test/java/Tests/OpenWeatherTest.java
+++ src/test/java/Tests/OpenWeatherTest.java
@@ -17,8 +17,44 @@ public class OpenWeatherTest {
                 .contentType("application/json; charset=utf-8");
     }
 
-    @Test(dependsOnMethods = "createWeatherStationTest()")
+    @Test(dependsOnMethods = "createWeatherStationTest")
     public void getCreatedWeatherStationTest(){
 
+        OpenWeatherRequestBuilder.getOpenWeatherResponse()
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .contentType("application/json; charset=utf-8");
+    }
+
+    @Test(dependsOnMethods = "getCreatedWeatherStationTest")
+    public void updateWeatherStationTest(){
+        OpenWeatherRequestBuilder.updateOpenWeatherResponse()
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .contentType("application/json; charset=utf-8");
+    }
+
+    @Test(dependsOnMethods = "updateWeatherStationTest")
+    public void deleteWeatherStationTest(){
+        OpenWeatherRequestBuilder.deleteOpenWeatherResponse()
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(204);
+    }
+
+    @Test(dependsOnMethods = "deleteWeatherStationTest")
+    public void CreatedWeatherStationWithoutNameNegativeTest(){
+
+        OpenWeatherRequestBuilder.createOpenWeatherWithoutNameResponse()
+                .then()
+//                .log().all()
+                .assertThat()
+                .statusCode(400)
+                .contentType("application/json; charset=utf-8");
     }
 }
