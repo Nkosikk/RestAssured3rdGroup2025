cee08c315a74ca6becb4b76b3b026f7aa8a68f87
Ourmine1010 <neo.ntwayaborwa@gmail.com>
2025-08-03 19:35:46 +0200

    Added TestDataGenerator class with its fields

--- src/test/java/Common/TestDataGenerator.java
+++ src/test/java/Common/TestDataGenerator.java
@@ -4,9 +4,9 @@ import com.github.javafaker.Faker;
 
 public class TestDataGenerator {
 
-    public static String longitude = Double.parseDouble(Faker.instance().address().longitude());
+    public static double longitude = Double.parseDouble(Faker.instance().address().longitude().replace(",", "."));
 
-    public static String latitude = Double.parseDouble(Faker.instance().address().latitude());
+    public static double latitude = Double.parseDouble(Faker.instance().address().latitude().replace(",", "."));
 
     public static int altitude = Faker.instance().number().numberBetween(10,20);
 
--- src/test/java/PayloadBuilder/OpenWeatherPayloadBuilder.java
+++ src/test/java/PayloadBuilder/OpenWeatherPayloadBuilder.java
@@ -5,14 +5,19 @@ import org.json.simple.JSONObject;
 
 public class OpenWeatherPayloadBuilder {
 
+    static double longitude = TestDataGenerator.longitude;
+    static double latitude = TestDataGenerator.latitude;
+    static double altitude = TestDataGenerator.altitude;
+
+
     public static JSONObject createWeatherStationBody(String externalId){
 
         JSONObject station = new JSONObject();
         station.put("external_id", externalId);
         station.put("name","First station");
-        station.put("latitude",37.76);
-        station.put("longitude",TestDataGenerator.longitude);
-        station.put("altitude",230);
+        station.put("latitude", latitude);
+        station.put("longitude", longitude);
+        station.put("altitude", altitude);
 
         return station;
     }
