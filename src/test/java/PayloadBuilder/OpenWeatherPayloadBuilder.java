de9886b1561ccef6fe32db12f83f7dd451a10e24
LubabaloMkhize <lubabalo.mkhize@gmail.com>
2026-02-03 20:12:05 +0200

    updated the request builder.

--- src/test/java/PayloadBuilder/OpenWeatherPayloadBuilder.java
+++ src/test/java/PayloadBuilder/OpenWeatherPayloadBuilder.java
@@ -46,14 +46,4 @@ public class OpenWeatherPayloadBuilder {
     }
 
 
-    public static JSONObject updateWeatherStationBody() {
-        JSONObject station = new JSONObject();
-        station.put("external_id", "updated_ext_id");
-        station.put("name", "Updated Station Name");
-        station.put("latitude", 38.00);
-        station.put("longitude", -123.00);
-        station.put("altitude", 250);
-        return station;
-    }
-
 }
